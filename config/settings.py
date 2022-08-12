dff343d9965b6b16fbcf5930a33f067a27756910
니꼬 <nico@nomadcoders.co>
2022-08-12 16:20:52 +0900

    5.1 Custom Model

--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-CUSTOM_APPS = [
-    "houses.apps.HousesConfig",
-]
+CUSTOM_APPS = ["houses.apps.HousesConfig", "users.apps.UsersConfig"]
 
 SYSTEM_APPS = [
     "django.contrib.admin",
@@ -127,3 +125,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Auth
+
+AUTH_USER_MODEL = "users.User"
--- houses/admin.py
+++ houses/admin.py
@@ -5,13 +5,5 @@ from .models import House
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
 
-    fields = (
-        "name",
-        "address",
-        ("price_per_night", "pets_allowed"),
-    )
     list_display = ("name", "price_per_night", "address", "pets_allowed")
     list_filter = ("price_per_night", "pets_allowed")
-    search_fields = ("address",)
-    list_display_links = ("name", "address")
-    list_editable = ("pets_allowed",)
