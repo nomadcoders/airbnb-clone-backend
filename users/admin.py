cc46b0eb557d93eac88e7e1ba237be3d6592d3ce
니꼬 <nico@nomadcoders.co>
2022-08-17 16:51:09 +0900

    5.4 Custom Admin

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,34 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": ("username", "password", "name", "email", "is_host"),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    list_display = ("username", "email", "name", "is_host")
