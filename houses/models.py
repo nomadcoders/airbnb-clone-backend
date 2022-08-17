d321d5e1555f410f65f80552c1f61441ec68d4e2
니꼬 <nico@nomadcoders.co>
2022-08-17 18:44:12 +0900

    5.5 Foreign Keys

--- houses/models.py
+++ houses/models.py
@@ -5,16 +5,25 @@ class House(models.Model):
 
     """Model Definition for Houses"""
 
-    name = models.CharField(max_length=140)
+    name = models.CharField(
+        max_length=140,
+    )
     price_per_night = models.PositiveIntegerField(
-        verbose_name="Price", help_text="Positive Numbers Only"
+        verbose_name="Price",
+        help_text="Positive Numbers Only",
     )
     description = models.TextField()
-    address = models.CharField(max_length=140)
+    address = models.CharField(
+        max_length=140,
+    )
     pets_allowed = models.BooleanField(
         verbose_name="Pets Allowed?",
         default=True,
         help_text="Does this house allow pets?",
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
     )
 
     def __str__(self):
--- users/models.py
+++ users/models.py
@@ -1,10 +1,20 @@
-from unittest.mock import DEFAULT
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
-    is_host = models.BooleanField(default=False)
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
