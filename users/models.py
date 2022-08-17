a7a5e13cc0576588b38f73be43354911e80202c6
니꼬 <nico@nomadcoders.co>
2022-08-17 16:18:57 +0900

    5.3 Defaults

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from unittest.mock import DEFAULT
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -5,5 +6,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150)
-    is_host = models.BooleanField()
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
