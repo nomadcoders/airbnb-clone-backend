17904781fcc43907bdfed40f2d4160bce6ea5fd8
니꼬 <nico@nomadcoders.co>
2022-08-18 15:46:44 +0900

    6.0 User Model

--- users/admin.py
+++ users/admin.py
@@ -9,7 +9,17 @@ class CustomUserAdmin(UserAdmin):
         (
             "Profile",
             {
-                "fields": ("username", "password", "name", "email", "is_host"),
+                "fields": (
+                    "avatar",
+                    "username",
+                    "password",
+                    "name",
+                    "email",
+                    "is_host",
+                    "gender",
+                    "language",
+                    "currency",
+                ),
                 "classes": ("wide",),
             },
         ),
--- users/models.py
+++ users/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
     first_name = models.CharField(
         max_length=150,
         editable=False,
@@ -11,8 +23,21 @@ class User(AbstractUser):
         max_length=150,
         editable=False,
     )
+    avatar = models.ImageField(blank=True)
     name = models.CharField(
         max_length=150,
         default="",
     )
-    is_host = models.BooleanField(null=True)
+    is_host = models.BooleanField(default=False)
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
