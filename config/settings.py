e1f6145470638af3728c7f569fac1fcd7009a303
니꼬 <nico@nomadcoders.co>
2022-08-18 16:06:05 +0900

    6.1 Room Model

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-CUSTOM_APPS = ["users.apps.UsersConfig"]
+CUSTOM_APPS = [
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+]
 
 SYSTEM_APPS = [
     "django.contrib.admin",
---
+++ rooms/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class Room(models.Model):
+
+    """Room Model Definition"""
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = "shared_room", "Shared Room"
+
+    country = models.CharField(
+        max_length=50,
+        default="한국",
+    )
+    city = models.CharField(
+        max_length=80,
+        default="서울",
+    )
+    price = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(
+        max_length=250,
+    )
+    pet_friendly = models.BooleanField(
+        default=True,
+    )
+    kind = models.CharField(
+        max_length=20,
+        choices=RoomKindChoices.choices,
+    )
+    owner = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+
+
+class Amenity(models.Model):
+
+    """Amenity Definiton"""
+
+    name = models.CharField(
+        max_length=150,
+    )
+    description = models.CharField(
+        max_length=150,
+        null=True,
+    )
