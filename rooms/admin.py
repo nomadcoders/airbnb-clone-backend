72dfce6eb5743cbf70e2ab3a4746826484672edc
니꼬 <nico@nomadcoders.co>
2022-08-31 12:51:08 +0900

    8.2 Admin Actions

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    actions = (reset_prices,)
 
     list_display = (
         "name",
