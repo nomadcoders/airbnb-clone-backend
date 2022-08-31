b653d44a6dc436ed9a879b511927c49e3da123b8
니꼬 <nico@nomadcoders.co>
2022-08-31 16:28:12 +0900

    9.0 Views

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rooms import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("rooms", views.say_hello),
 ]
