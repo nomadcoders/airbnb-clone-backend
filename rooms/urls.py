da052e0b8448fc5bcfce662036baf6a1b78e8786
니꼬 <nico@nomadcoders.co>
2022-08-31 16:55:12 +0900

    9.2 URL Arguments

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_hello),
+    path("", views.see_all_rooms),
+    path("<int:room_id>", views.see_one_room),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("hello!")
+def see_all_rooms(request):
+    return HttpResponse("see all rooms")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"see room with id: {room_id}")
