e9adbeb1de842d21b5c64c12ab8f1b7775ec849f
니꼬 <nico@nomadcoders.co>
2022-09-02 15:36:39 +0900

    10.0 Introduction

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 
 CUSTOM_APPS = [
     "common.apps.CommonConfig",
@@ -52,7 +55,7 @@ SYSTEM_APPS = [
     "django.contrib.staticfiles",
 ]
 
-INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
