784d414f3b6432c5fbe3934d62ab56cfc165adde
니꼬 <nico@nomadcoders.co>
2022-09-02 16:03:46 +0900

    10.2 api_view

--- categories/views.py
+++ categories/views.py
@@ -1,12 +1,13 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse(
+    return Response(
         {
             "ok": True,
-            "categories": all_categories,
+            "categories": Category.objects.all(),
         }
     )
