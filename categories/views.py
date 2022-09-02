86aed96f9382332844a5cac9030c8c1ef0435273
니꼬 <nico@nomadcoders.co>
2022-09-02 15:51:49 +0900

    10.1 JsonResponse

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Category
 
-# Create your views here.
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse(
+        {
+            "ok": True,
+            "categories": all_categories,
+        }
+    )
--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("rooms/", include("rooms.urls")),
+    path("categories/", include("categories.urls")),
 ]
