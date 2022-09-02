359401cdbe93868c43de6eca730ba9567cb79ba7
니꼬 <nico@nomadcoders.co>
2022-09-02 16:55:34 +0900

    10.5 serializer.is_valid()

--- categories/serializers.py
+++ categories/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 
 class CategorySerializer(serializers.Serializer):
 
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created_at = serializers.DateTimeField()
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
+    created_at = serializers.DateTimeField(read_only=True)
--- categories/views.py
+++ categories/views.py
@@ -11,11 +11,11 @@ def categories(request):
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
     elif request.method == "POST":
-        Category.objects.create(
-            name=request.data["name"],
-            kind=request.data["kind"],
-        )
-        return Response({"created": True})
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"created": True})
+        else:
+            return Response(serializer.errors)
 
 
 @api_view()
