56b1eb89b184b83ec29e367b0795db503580f6ad
니꼬 <nico@nomadcoders.co>
2022-09-13 18:27:14 +0900

    11.25 Booking Completed

--- rooms/views.py
+++ rooms/views.py
@@ -229,6 +229,12 @@ class RoomBookings(APIView):
         room = self.get_object(pk)
         serializer = CreateRoomBookingSerializer(data=request.data)
         if serializer.is_valid():
-            return Response({"ok": True})
+            booking = serializer.save(
+                room=room,
+                user=request.user,
+                kind=Booking.BookingKindChoices.ROOM,
+            )
+            serializer = PublicBookingSerializer(booking)
+            return Response(serializer.data)
         else:
             return Response(serializer.errors)
