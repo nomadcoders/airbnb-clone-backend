8ea4c19c71207f1deda22d0bf20f5ad91c5a9188
니꼬 <nico@nomadcoders.co>
2022-09-13 19:56:08 +0900

    12.1 Create User

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
+    path("change-password", views.ChangePassword.as_view()),
+    path("log-in", views.LogIn.as_view()),
+    path("log-out", views.LogOut.as_view()),
+    path("@<str:username>", views.PublicUser.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError, NotFound
 from rest_framework.permissions import IsAuthenticated
+
+from users.models import User
 from . import serializers
 
 
@@ -23,6 +27,22 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
