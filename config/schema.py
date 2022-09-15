f9749078db29423abbcd0705395287ce1a45c2e8
니꼬 <nico@nomadcoders.co>
2022-09-15 16:56:08 +0900

    14.4 Refactor

--- config/schema.py
+++ config/schema.py
@@ -15,29 +15,34 @@ movies_db = [
 ]
 
 
+def movies():
+    return movies_db
+
+
+def movie(movie_pk: int):
+    return movies_db[movie_pk - 1]
+
+
 @strawberry.type
 class Query:
-    @strawberry.field
-    def movies(self) -> typing.List[Movie]:
-        return movies_db
+    movies: typing.List[Movie] = strawberry.field(resolver=movies)
+    movie: Movie = strawberry.field(resolver=movie)
 
-    @strawberry.field
-    def movie(self, movie_pk: int) -> Movie:
-        return movies_db[movie_pk - 1]
+
+def add_movie(title: str, year: int, rating: int):
+    new_movie = Movie(
+        pk=len(movies_db) + 1,
+        title=title,
+        year=year,
+        rating=rating,
+    )
+    movies_db.append(new_movie)
+    return new_movie
 
 
 @strawberry.type
 class Mutation:
-    @strawberry.mutation
-    def add_movie(self, title: str, year: int, rating: int) -> Movie:
-        new_movie = Movie(
-            pk=len(movies_db) + 1,
-            title=title,
-            year=year,
-            rating=rating,
-        )
-        movies_db.append(new_movie)
-        return new_movie
+    add_movie: Movie = strawberry.mutation(resolver=add_movie)
 
 
 schema = strawberry.Schema(
