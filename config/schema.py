076304e5612b40bef2c35e6b961176d4fb217027
니꼬 <nico@nomadcoders.co>
2022-09-15 16:17:35 +0900

    14.2 Query Arguments

--- config/schema.py
+++ config/schema.py
@@ -1,11 +1,29 @@
 import strawberry
+import typing
+
+
+@strawberry.type
+class Movie:
+    pk: int
+    title: str
+    year: int
+    rating: int
+
+
+movies_db = [
+    Movie(pk=1, title="Godfather", year=1990, rating=10),
+]
 
 
 @strawberry.type
 class Query:
     @strawberry.field
-    def ping(self) -> str:
-        return "pong"
+    def movies(self) -> typing.List[Movie]:
+        return movies_db
+
+    @strawberry.field
+    def movie(self, movie_pk: int) -> Movie:
+        return movies_db[movie_pk - 1]
 
 
 schema = strawberry.Schema(query=Query)
