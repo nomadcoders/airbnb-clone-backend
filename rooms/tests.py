beb20ed214f2050d2165079694105030e0113568
니꼬 <nico@nomadcoders.co>
2022-09-17 19:22:07 +0900

    16.3 Create Amenity Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -7,6 +7,8 @@ class TestAmenities(APITestCase):
     NAME = "Amenity Test"
     DESC = "Amenity Des"
 
+    URL = "/api/v1/rooms/amenities/"
+
     def setUp(self):
         models.Amenity.objects.create(
             name=self.NAME,
@@ -15,7 +17,7 @@ class TestAmenities(APITestCase):
 
     def test_all_amenities(self):
 
-        response = self.client.get("/api/v1/rooms/amenities/")
+        response = self.client.get(self.URL)
         data = response.json()
 
         self.assertEqual(
@@ -39,3 +41,37 @@ class TestAmenities(APITestCase):
             data[0]["description"],
             self.DESC,
         )
+
+    def test_create_amenity(self):
+
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Amenity desc."
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
+        )
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
+        self.assertEqual(
+            data["name"],
+            new_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            new_amenity_description,
+        )
+
+        response = self.client.post(self.URL)
+        data = response.json()
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("name", data)
--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 from django.db import transaction
-from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
 from rest_framework.exceptions import (
     NotFound,
@@ -33,7 +33,10 @@ class Amenities(APIView):
                 serializers.AmenitySerializer(amenity).data,
             )
         else:
-            return Response(serializer.errors)
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
 
 
 class AmenityDetail(APIView):
