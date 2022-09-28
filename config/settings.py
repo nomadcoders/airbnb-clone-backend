8b3e7ce93ab952564c14324deba4b071b2ddbbdf
니꼬 <nico@nomadcoders.co>
2022-09-28 17:38:24 +0900

    onrender.com

--- config/settings.py
+++ config/settings.py
@@ -221,7 +221,7 @@ if not DEBUG:
     SESSION_COOKIE_HTTPONLY = False
     CSRF_COOKIE_HTTPONLY = False
     SESSION_COOKIE_SECURE = False
-    SESSION_COOKIE_DOMAIN = ".nomadsubs.com"
+
     sentry_sdk.init(
         dsn="https://5344b5a4537c4c7bb8a18457d9905e05@o285966.ingest.sentry.io/4503890242961408",
         integrations=[
