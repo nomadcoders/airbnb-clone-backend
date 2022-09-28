09b4d3e4473c0626aa68f0b7cf592afef88f1a00
니꼬 <nico@nomadcoders.co>
2022-09-28 18:25:27 +0900

    Localhost

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = [
     "localhost",
+    "backend.airbnbclonetest.xyz",
 ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
